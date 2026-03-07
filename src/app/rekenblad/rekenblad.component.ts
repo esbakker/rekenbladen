80df5d0769a33295a01d11b9419af70c560e00a1
eddy <eddy@e-bakker.net>
2026-03-07 12:55:28 +0100

    fix build

--- src/app/rekenblad/rekenblad.component.ts
+++ src/app/rekenblad/rekenblad.component.ts
@@ -2,18 +2,16 @@ import {Component} from '@angular/core';
 import {GenerateSumService} from '../shared/services/generate-sum.service';
 import {ConfigService} from '../shared/services/config.service';
 import {UiStateService} from '../shared/services/ui-state.service';
-import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-rekenblad',
-  imports: [
-    NgClass
-  ],
+  imports: [],
   templateUrl: './rekenblad.component.html',
   styleUrl: './rekenblad.component.scss',
 })
 export class RekenbladComponent {
 
-  constructor(readonly generateSumService: GenerateSumService, readonly configService: ConfigService, readonly uiStateService: UiStateService) {
+  constructor(readonly generateSumService: GenerateSumService, readonly configService: ConfigService,
+              readonly uiStateService: UiStateService) {
   }
 }
